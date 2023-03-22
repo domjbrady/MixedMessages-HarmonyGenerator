11d00542adcec9fb0fe7168d833f39e4e46fc128
Dom Brady <dom.brades@gmail.com>
2023-03-22 12:19:59 +0000

    add verse/chorus/bridge generators

--- main.js
+++ main.js
@@ -1,6 +1,5 @@
 //MIXED MESSAGES PORTFOLIO PROJECT
 
-
 const randomTone = () => {
     const tones = ["C", "C#/Db", "D", "D#/Eb", "E", "F", "F#/Gb", "G", "G#/Ab", "A", "A#/Bb", "B"];
     return tones[Math.floor(Math.random()*12)]
@@ -11,3 +10,32 @@ const randomQuality = () => {
     return qualities[Math.floor(Math.random()*16)]
 }
 
+const chordGenerator = () => {
+    return randomTone() + randomQuality();
+}
+
+const verseGenerator = (verseLength) => {
+    const verseArr = [];
+    for (let i = 0; i <= verseLength - 1; i++){
+        verseArr.push(chordGenerator())
+    }
+    const verse = verseArr.join("  ");
+    return `VERSE: ${verse}`
+}
+const chorusGenerator = (chorusLength) => {
+    const chorusArr = [];
+    for (let i = 0; i <= chorusLength - 1; i++){
+        chorusArr.push(chordGenerator())
+    }
+    const chorus = chorusArr.join("  ");
+    return `CHORUS: ${chorus}`
+}
+
+const bridgeGenerator = (bridgeLength) => {
+    const bridgeArr = [];
+    for (let i = 0; i <= bridgeLength - 1; i++){
+        bridgeArr.push(chordGenerator())
+    }
+    const bridge = bridgeArr.join("  ");
+    return `BRIDGE: ${bridge}`
+}
