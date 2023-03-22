abff64b66f6c473a221b9e576879fd4a39ae49aa
Dom Brady <dom.brades@gmail.com>
2023-03-22 11:47:23 +0000

    add tone/quality randomisers

--- main.js
+++ main.js
@@ -1,1 +1,13 @@
 //MIXED MESSAGES PORTFOLIO PROJECT
+
+
+const randomTone = () => {
+    const tones = ["C", "C#/Db", "D", "D#/Eb", "E", "F", "F#/Gb", "G", "G#/Ab", "A", "A#/Bb", "B"];
+    return tones[Math.floor(Math.random()*12)]
+}
+
+const randomQuality = () => {
+    const qualities = ["7", "9", "11", "13", "alt", "", "6", "maj7", "maj9", "maj13", "-", "-7", "-9", "-11", "aug", "dim"];
+    return qualities[Math.floor(Math.random()*16)]
+}
+
